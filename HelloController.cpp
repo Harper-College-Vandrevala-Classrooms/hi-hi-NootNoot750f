1c40412d9398f017d8ecda3706967da074323fe3
NootNoot750f <nickholas195@gmail.com>
2024-09-25 23:14:40 -0500

    Testing

--- HelloController.cpp
+++ HelloController.cpp
@@ -1,3 +1,19 @@
+//Nickholas Gherman
+//Objective: Make a program that compares 2 strings to each other and I forgot
+
 using namespace std;
 #include <string>
-//Test
+#include <iostream>
+
+int main() {
+	string string1 = "";
+	string string2 = "";
+
+	cout << "Input a string: ";
+	cin >> string1;
+	cout << endl;
+	cout << "Enter another string: ";
+	cin >> string2;
+	cout << endl;
+
+}
