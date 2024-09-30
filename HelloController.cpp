1bdea533416c263e1f5a4258e16b203631d79669
NootNoot750f <nickholas195@gmail.com>
2024-09-29 20:00:04 -0500

    This works!

--- HelloController.cpp
+++ HelloController.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 #include <string>
 #include <iostream>
+#include <cctype>
+
+
+int strcmp_case_insensitive(string& string1, string& string2);
 
 int main() {
 	string string1 = "";
@@ -15,6 +19,23 @@ int main() {
 	cout << "Enter another string: ";
 	cin >> string2;
 	cout << endl;
-//Does this work now?
-//Testing on github
+
+	cout << "The comparison of " << string1 << " and " << string2 << ":" << strcmp_case_insensitive(string1, string2) << endl;
+
+return 0;
+}
+
+
+int strcmp_case_insensitive(string& string1, string& string2) {
+	for (int i = 0; i < string1.length() && i < string2.length(); i++) {
+		char char1 = tolower(string1[i]);
+		char char2 = tolower(string2[i]);
+		if (char1 != char2) {
+			return char1 - char2;
+		}
+	}
+	if(string1.length() != string2.length()) {
+		return string1.length() - string2.length();
+	}
+	return 0;
 }
